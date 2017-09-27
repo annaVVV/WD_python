ddfbbea89e62ae8f847fc2780401993e4cb1bc69
annaVVV <vergeles.anna@gmail.com>
2017-09-27 08:25:30 -0700

    Examples of waits

---
+++ lib/wait_example.py
@@ -0,0 +1,81 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import *
+
+# https://stackoverflow.com/questions/25727340/javas-fluentwait-in-python
+# fluent wait
+
+driver = webdriver.Firefox()
+driver.get("http://somedomain/url_that_delays_loading")
+wait = WebDriverWait(driver, 10, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
+element = wait.until(EC.element_to_be_clickable((By.XPATH, "//div")))
+
+
+# http://selenium-python.readthedocs.io/waits.html
+# Explicit Waits
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "myDynamicElement"))
+    )
+finally:
+    driver.quit()
+    
+"""
+Expected Conditions
+
+title_is
+title_contains
+presence_of_element_located
+visibility_of_element_located
+visibility_of
+presence_of_all_elements_located
+text_to_be_present_in_element
+text_to_be_present_in_element_value
+frame_to_be_available_and_switch_to_it
+invisibility_of_element_located
+element_to_be_clickable
+staleness_of
+element_to_be_selected
+element_located_to_be_selected
+element_selection_state_to_be
+element_located_selection_state_to_be
+alert_is_present
+"""
+
+wait = WebDriverWait(driver, 10)
+element = wait.until(EC.element_to_be_clickable((By.ID, 'someid')))
+
+
+
+# Custom Conditions
+
+class element_has_css_class(object):
+  """An expectation for checking that an element has a particular css class.
+
+  locator - used to find the element
+  returns the WebElement once it has the particular css class
+  """
+  def __init__(self, locator, css_class):
+    self.locator = locator
+    self.css_class = css_class
+
+  def __call__(self, driver):
+    element = driver.find_element(*self.locator)   # Finding the referenced element
+    if self.css_class in element.get_attribute("class"):
+        return element
+    else:
+        return False
+
+# Wait until an element with id='myNewInput' has class 'myCSSClass'
+wait = WebDriverWait(driver, 10)
+element = wait.until(element_has_css_class((By.ID, 'myNewInput'), "myCSSClass"))
+
+#Implicit Waits
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(10) # seconds
+driver.get("http://somedomain/url_that_delays_loading")
+myDynamicElement = driver.find_element_by_id("myDynamicElement")    
